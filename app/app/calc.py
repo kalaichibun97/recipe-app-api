7e43e0e58944f284e2bc071254120701f3e73bf0
Bao Nguyen <98855989+kalaichibun97@users.noreply.github.com>
2025-05-13 16:07:09 +0700

    Configure docker compose and check to wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Simple calculator functions
+"""
+
+
+def add(a, b):
+    """Add x and y and return the result"""
+    return a + b
+
+
+def subtract(a, b):
+    """Subtract y from x and return the result"""
+    return a - b
